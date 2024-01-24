c6ab0f686d40e248ba88628c0aa99ad9fd981bb5
Trueno110 <Trueno110@yandex.ru>
2024-01-24 12:07:21 +0300

    Добавьте файлы проекта.

---
+++ Variant_2_Kontrol_Work/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Variant_2_Kontrol_Work
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+        Maneger man = new Maneger();
+            man.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student();
+            student.Show();
+            this.Close();
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            Teachers teacher = new Teachers();
+            teacher.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Variant_2_Kontrol_Work/Student.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Variant_2_Kontrol_Work
+{
+    /// <summary>
+    /// Логика взаимодействия для Student.xaml
+    /// </summary>
+    public partial class Student : Window
+    {
+        public Student()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
